1e3bf4cc07e41b5e1280171f007ada9ef8103aa5
Undefined011001 <148182218+Undefined011001@users.noreply.github.com>
2023-12-25 09:26:57 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+	
+conn = psycopg2.connect(
+        host = '78.141.227.124',
+        user = 'postgres',
+        database = 'GITHUB',
+        password = '123')
+print('Подключение успешно')
+
+cur  = conn.cursor()
+
+# cur.execute('SELECT * FROM students')
+# # students = cur.fetchall()
+# cur.execute('SELECT COUNT(*) FROM students WHERE age > 15 ')
+# students = cur.fetchall()
+
+cur.execute("SELECT AVG(age) FROM students WHERE stud_name LIKE 'A%'")
+students = cur.fetchall()
+
+for i in students: 
+    print(i)
+    
+    
